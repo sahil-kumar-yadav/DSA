3ede0fcc7f66c0eafb99c5caa4fb3e8e235a6d4a
yadavsk098 <yadavsk098@gmail.com>
2024-07-24 11:33:41 +0530

    nth root of number

--- Basic Maths/squareRoot-of-a-number.cpp
+++ Basic Maths/squareRoot-of-a-number.cpp
@@ -2,17 +2,25 @@
 
 using namespace std;
 
+double multiply(double mid,int n){
+    int ans = 1;
+    while(n--){
+        ans *= mid;
+    }
+    return ans;
+}
 
 int main()
 {
     double esp = 1e-6;
-    double x = 121;
+    double x = 4;
     double lo = 0, hi = x, mid;
+    int n = 2;
 
     while (hi - lo > esp) // to get precision till esp decimal
     {
         mid = (hi + lo)/2;
-        if(mid * mid < x){
+        if(multiply(mid,n) < x){
             lo = mid;
         }
         else{
