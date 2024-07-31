2f66c4cb75a7ea56a3c3607ccc120aae4f47cff1
yadavsk098 <yadavsk098@gmail.com>
2024-07-31 22:20:00 +0530

    common element in three array

--- Array/common-element-in-three-sorted-array.cpp
+++ Array/common-element-in-three-sorted-array.cpp
@@ -2,10 +2,44 @@
 
 using namespace std;
 
-void commonElement(int a[], int b[], int c[], int na, int nb, int nc)
+vector<int> commonElement(int a[], int b[], int c[], int na, int nb, int nc)
 {
-    
+    vector<int> ans;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < a.size() && j < b.size() && k < c.size())
+    {
+        if (a[i] == b[j] && b[j] == c[k])
+        {
+            if (ans.empty() || ans.back() != a[i])
+            {
+                ans.push_back(a[i]);
+            }
+            i++;
+            j++;
+            k++;
+        }
+        else if (a[i] < b[j])
+        {
+            i++;
+        }
+        else if (b[j] < c[k])
+        {
+            j++;
+        }
+        else
+        {
+            k++;
+        }
+    }
+    return ans;
+
+    // cout<<"completed"<<endl;
 }
+
+
 int main()
 {
     int a[] = {1, 5, 10, 20, 40, 80};
