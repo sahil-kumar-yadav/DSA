da5f27a73d8b0370281530d98143ef164f776e49
yadavsk098 <yadavsk098@gmail.com>
2024-07-31 22:19:45 +0530

    common element in three sorted array

---
+++ Array/common-element-in-three-sorted-array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void commonElement(int a[], int b[], int c[], int na, int nb, int nc)
+{
+    
+}
+int main()
+{
+    int a[] = {1, 5, 10, 20, 40, 80};
+    int b[] = {6, 7, 20, 80, 100};
+    int c[] = {3, 4, 15, 20, 30, 70, 80, 120};
+
+    int na = 6, nb = 5, nc = 8;
+
+    commonElement(a, b, c, na, nb, nc);
+
+    return 0;
+}
