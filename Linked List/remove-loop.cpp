5aa7d2a3a39c3d6399f0f74e03758f34b828adba
yadavsk098 <yadavsk098@gmail.com>
2024-08-07 20:22:01 +0530

    Remove LL from Loop

---
+++ Linked List/remove-loop.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int x)
+    {
+        this->data = x;
+        this->next = NULL;
+    }
+};
+
+void printLL(Node *root)
+{
+
+    Node *temp = root;
+
+    while (temp)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+bool CheckCycle(Node *head)
+{
+    if (!head)
+        return false;
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL)
+    {
+        fast = fast->next;
+
+        if (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        if (slow == fast)
+        {
+            return true; // cycle present
+        }
+    }
+
+    return false;
+}
+
+void removeCycle(Node *head)
+{
+    if (!head)
+        return ;
+
+    Node *slow = head;
+    Node *fast = head;
+    Node *prev = fast;
+
+    while (slow != fast)
+    {
+        prev = fast;
+        fast = fast->next;
+
+        if (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }        
+    }
+
+    prev->next = NULL;
+
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+    Node *fourth = new Node(40);
+    Node *fifth = new Node(50);
+    Node *sixth = new Node(60);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = third;
+    
+    bool isCycle = CheckCycle(head);
+    if (isCycle)
+    {
+        cout << "Cycle Present " << endl;
+    }
+    else
+    {
+        cout << "Cycle Not present " << endl;
+    }
+
+    removeCycle(head);
+    cout<<"here"<<endl;
+    isCycle = CheckCycle(head);
+
+    if (isCycle)
+    {
+        cout << "Cycle Present " << endl;
+    }
+    else
+    {
+        cout << "Cycle Not present " << endl;
+    }
+
+    return 0;
+}
