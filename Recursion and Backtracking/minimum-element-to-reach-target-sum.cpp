b8e2d9ccf3ad995982f7279235ef932474e4dcc1
yadavsk098 <yadavsk098@gmail.com>
2024-08-07 20:23:19 +0530

    minimum number of element to achive target sum

--- Recursion and Backtracking/minimum-element-to-reach-target-sum.cpp
+++ Recursion and Backtracking/minimum-element-to-reach-target-sum.cpp
@@ -27,13 +27,39 @@ int findMinimumElement(int arr[], int n, int target)
     return mini;
 }
 
+int findMinimumElementBottomUp(int arr[], int n, int target, int sum)
+{
+    if (sum == target)
+    {
+        return 0;
+    }
+
+    if (sum > target)
+    {
+        return INT_MAX;
+    }
+    int mini = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        int ans = findMinimumElementBottomUp(arr, n, target, sum + arr[i]);
+        if(ans != INT_MAX)
+            mini = min(mini, ans + 1);
+    }
+
+    return mini;
+}
+
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5};
     int n = 5;
     int target = 11;
 
-    int ans = findMinimumElement(arr, n, target);
+    // int ans = findMinimumElement(arr, n, target);
+
+    int sum = 0;
+    int ans = findMinimumElementBottomUp(arr, n, target, sum);
+
     cout << "ans " << ans << endl;
 
     return 0;
