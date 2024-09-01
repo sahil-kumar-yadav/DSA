5194d01da4dc150c735f458ebdd83ab6e034bafc
yadavsk098 <yadavsk098@gmail.com>
2024-09-01 07:57:23 +0530

    longest common prefix

--- 03_String and Char array/Longest-Common-Prefix-of-Strings.cpp
+++ 03_String and Char array/Longest-Common-Prefix-of-Strings.cpp
@@ -19,8 +19,10 @@ bool checkInAll(vector<string> arr, string &str)
     for (auto it : arr)
     {
         // cout << "it " << it << " str " << str << endl;
+        if (it.size() < str.size())
+            return false;
         size_t found = it.find(str);
-        if (found == string::npos ||  found != 0)
+        if (found == string::npos || found != 0)
         {
             // cout << str << " not found in " << it;
             return false;
@@ -29,13 +31,10 @@ bool checkInAll(vector<string> arr, string &str)
     return true;
 }
 
-int main()
+string longestCommonPrefix(vector<string> &arr)
 {
-    // vector<string> arr = {"geeksforgeeks", "geeks", "geek", "geezer"};
-    vector<string> arr = {"ol" ,"xozic"};
     int n = arr.size();
 
-    printArr(arr);
     int smallestIndex = 0;
     int len = arr[0].length();
     for (int i = 0; i < n; i++)
@@ -48,19 +47,40 @@ int main()
         }
     }
 
-    string smallest = arr[smallestIndex];
-    cout << smallest << endl;
+    // string smallest = arr[smallestIndex];
+    string smallest = arr[0];
+    // cout << smallest << endl;
     // int strlen = smallest.length();
-    cout << "Now ans" << endl;
+
+    // cout << "Now ans" << endl;
     for (int i = smallest.length() - 1; i >= 0; i--)
     {
         string newstr = smallest.substr(0, i + 1 - 0);
-        cout << "newstr " << newstr << endl;
+        // cout << "newstr " << newstr << endl;
         if (checkInAll(arr, newstr))
         {
-            cout << newstr << " --" << endl;
+            // cout << newstr << " --" << endl;
+            return newstr;
+            break;
         }
     }
+
+    return "";
+}
+
+int main()
+{
+    // vector<string> arr = {"geeksforgeeks", "geeks", "geek", "geezer"};
+    // vector<string> arr = {"ol" ,"xozic"};
+    // vector<string> arr = {"flower", "flow", "flight"};
+    // vector<string> arr = {"dog", "racecar", "car"};
+    // vector<string> arr = {"c","acc","ccc"}; // ans empty hai
+
+    // printArr(arr);
+    cout<<"Printing ans "<<endl;
+    string ans = longestCommonPrefix(arr);
+
+    cout<<ans<<endl;
 
     return 0;
 }
