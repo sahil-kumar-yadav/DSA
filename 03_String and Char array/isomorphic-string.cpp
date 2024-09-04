59ee79cb7d7d6ac0f6d802e6bb2e83e7ebc25bf4
yadavsk098 <yadavsk098@gmail.com>
2024-09-04 21:30:21 +0530

    isomorphic string

---
+++ 03_String and Char array/isomorphic-string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<string>
+#include<unordered_map>
+using namespace std;
+int main()
+{
+    // Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+    //     nput: s = "egg", t = "add"
+
+    //     Output: true
+
+    //     Explanation:
+
+    //     The strings s and t can be made identical by:
+
+    //     Mapping 'e' to 'a'.
+    //     Mapping 'g' to 'd'.
+
+
+
+    return 0;
+}
